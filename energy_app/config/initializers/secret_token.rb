67894f5c0f078cc40fb9a88b79c71007f56e4093
Mike Demmitt <mdemmitt@gmail.com>
2014-02-04 19:31:37 -0800

    First commit

---
+++ energy_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnergyApp::Application.config.secret_key_base = 'ef926645dc36f290072f86fca81439080624520db6c71ff20ee5ceca2ebddd5def2fe2629f532d69af0a27854823c11c1514c63e38d34d657046752fdf69009e'
---
+++ energy_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnergyApp::Application.config.session_store :cookie_store, key: '_energy_app_session'
