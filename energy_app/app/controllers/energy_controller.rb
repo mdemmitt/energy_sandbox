1e4bc07528adc6fc2d58ac89a327cdc19f8d104f
Mike Demmitt <mdemmitt@gmail.com>
2014-02-06 12:46:00 -0800

    successfully display all redis keys in the index view

--- energy_app/app/controllers/energy_controller.rb
+++ energy_app/app/controllers/energy_controller.rb
@@ -1,5 +1,17 @@
 class EnergyController < ApplicationController
   def index
-    @keys = $redis.keys
+    keys = $redis.keys
+    @stringKeys = []
+    @hashKeys = []
+    @types = []
+    keys.each do |key|
+      type = $redis.type key
+      @types << type
+      if type.eql? "string"
+        @stringKeys << key
+      elsif type.eql? "hash"
+        @hashKeys << key
+      end
+    end
   end
 end
